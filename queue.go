4cc1db9f68fc2c7eea6453a07c7cf3be42fb4399
Chris Harrington <chris.harrington@startribune.com>
2021-05-11 14:22:34 -0500

    Rework how we do statistics and show diagnostics

--- queue.go
+++ queue.go
@@ -25,20 +25,35 @@ type queueCmd struct {
 }
 
 type queueResult struct {
-	err error
+	cmd       *queueCmd
+	changed   bool
+	skipped   bool
+	succeeded bool
+	err       error
+	warn      error
 }
 
 func queueDrainer(res <-chan *queueResult) {
-	var success, fail int
+	var downloaded, changed, skipped, failed, warned int
 	fmt.Fprintf(os.Stderr, "\nTransfer results:\n")
 	for r := range res {
 		if r.err != nil {
-			fail++
-			fmt.Fprintf(os.Stderr, "\n%v\n", r.err)
-		} else {
-			success++
+			failed++
+			fmt.Fprintf(os.Stderr, "\r%v\n%+v\n", r.err, *r.cmd)
+		} else if r.warn != nil {
+			warned++
+			fmt.Fprintf(os.Stderr, "\r%v\n%+v\n", r.warn, *r.cmd)
 		}
-		fmt.Fprintf(os.Stderr, "\r%d failed, %d succeeded       \r", fail, success)
+
+		if r.changed {
+			changed++
+		} else if r.skipped {
+			skipped++
+		} else if r.succeeded {
+			downloaded++
+		}
+		fmt.Fprintf(os.Stderr, "\r%d failed, %d warned, %d new, %d changed, %d skipped       \r",
+			failed, warned, downloaded, changed, skipped)
 	}
 	fmt.Fprintf(os.Stderr, "\n\nDone\n\n")
 }
@@ -61,6 +76,7 @@ func startQueue(sess *session.Session, xmlin io.ReadSeeker) error {
 	}
 	wg.Wait()
 	close(out)
+	time.Sleep(1 * time.Second) // ick, sorry
 	return nil
 }
 
@@ -72,7 +88,7 @@ func queueRunner(wg *sync.WaitGroup, sess *session.Session, inChan <-chan *queue
 }
 
 func queueItem(sess *session.Session, in *queueCmd) (out *queueResult) {
-	out = &queueResult{}
+	out = &queueResult{cmd: in}
 	s3api := s3.New(sess)
 
 	// see if the object is already in s3, and if it is, get its length
@@ -83,7 +99,7 @@ func queueItem(sess *session.Session, in *queueCmd) (out *queueResult) {
 	}
 
 	// if the item exists in s3, do a HEAD request against the source
-	if s3ObjLength >= 0 {
+	if s3ObjLength > 0 {
 		retries := 3
 		for {
 			headRes, err := http.Head(in.srcURL)
@@ -104,21 +120,27 @@ func queueItem(sess *session.Session, in *queueCmd) (out *queueResult) {
 				}
 			}
 			// zero-length files aren't a thing
-			if headRes.ContentLength == 0 {
+			if headRes.ContentLength <= 0 {
 				time.Sleep(1 * time.Second)
 				if retries > 0 {
 					retries--
 					continue
 				}
-			}
-			if headRes.StatusCode != 200 {
-				out.err = fmt.Errorf("source URL %q returned HEAD status %03d", in.srcURL, headRes.StatusCode)
+				out.err = fmt.Errorf("source URL %q returned HEAD size %d", in.srcURL, headRes.ContentLength)
 				return
 			}
-			if headRes.ContentLength == s3ObjLength {
+			if headRes.StatusCode != 200 {
+				out.warn = fmt.Errorf("source URL %q returned HEAD status %03d", in.srcURL, headRes.StatusCode)
+				// we can just try download it in any case
+			} else if headRes.ContentLength == s3ObjLength {
 				out.err = nil
+				out.skipped = true
 				return // object in s3 is the same length as source, we're done
+			} else if headRes.ContentLength < s3ObjLength {
+				out.warn = fmt.Errorf("source %q is smaller than s3 content (%d < %d)",
+					in.srcURL, headRes.ContentLength, s3ObjLength)
 			}
+			out.changed = true
 			break
 		}
 	}
@@ -148,5 +170,6 @@ func queueItem(sess *session.Session, in *queueCmd) (out *queueResult) {
 		out.err = fmt.Errorf("cannot upload to s3: %w", err)
 		return
 	}
+	out.succeeded = true
 	return
 }
