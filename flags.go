b6823cca0327da7fee91b0fa37ef62112c730b65
Chris Harrington <chris.harrington@startribune.com>
2021-05-11 01:04:02 -0500

    api wants plain bucket names, no protocol

--- flags.go
+++ flags.go
@@ -28,7 +28,7 @@ func init() {
 	flag.StringVar(&xmlFile, "xmlfile", "", "XML file to ingest for transfer")
 	flag.StringVar(&awsRegion, "region", env("AWS_REGION", "us-west-2"), "AWS Region")
 	flag.StringVar(&srcHTTPRoot, "src", "https://www.example.net", "Source file location")
-	flag.StringVar(&dstS3Bucket, "dst", "s3://exampleBucket", "Destination S3 Bucket")
+	flag.StringVar(&dstS3Bucket, "dst", "exampleBucket", "Destination S3 Bucket")
 	flag.StringVar(&dstS3Path, "dstPath", "/", "Destination S3 Bucket Path")
 	flag.StringVar(&tmpLocation, "tmp", os.TempDir(), "Location for storing files until upload")
 	flag.IntVar(&maxThreads, "threads", 1, "Maximum number of items to download/upload in parallel")
@@ -58,11 +58,6 @@ func init() {
 
 	if strings.HasSuffix(dstS3Bucket, "/") {
 		fmt.Fprintf(os.Stderr, "The destination bucket (%q) must not end in a forward slash\n", dstS3Bucket)
-		os.Exit(1)
-	}
-
-	if !strings.HasPrefix(dstS3Bucket, "s3://") {
-		fmt.Fprintf(os.Stderr, "The destination bucket (%q) must start with \"s3://\"\n", dstS3Bucket)
 		os.Exit(1)
 	}
 
