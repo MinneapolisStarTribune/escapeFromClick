e069223f69be48c1764a10f5822902ea01c38099
Chris Harrington <chris.harrington@startribune.com>
2021-05-11 14:22:49 -0500

    It's possible for ContentLength to be zero if the body length is unknown

--- http.go
+++ http.go
@@ -18,9 +18,6 @@ func curl(w io.WriteSeeker, u string) error {
 	if h.StatusCode != 200 {
 		return fmt.Errorf("source URL %q returned GET status %03d", u, h.StatusCode)
 	}
-	if h.ContentLength == 0 {
-		return fmt.Errorf("content length of %q is supposedly zero", u)
-	}
 	nbytes, err := io.Copy(w, h.Body)
 	if err != nil {
 		return fmt.Errorf("failed while copying contents of %q: %w", u, err)
